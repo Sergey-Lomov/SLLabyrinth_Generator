fc76d37df82e2da7ede2799d8f31a86c47ed04fd
serhii.lomov <serhii.lomov@itomy.ch>
2025-03-21 17:32:58 +0200

    Fixed one way holder collapse fail

--- Sources/SLLabyrinthGenerator/Elements/Concrete/OneWayHolder.swift
+++ Sources/SLLabyrinthGenerator/Elements/Concrete/OneWayHolder.swift
@@ -60,7 +60,7 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
     static var weigthCategory: String { "one_way_holder" }
 
     private var wallsVariations = initialState()
-    private var unavailable: [Edge] = []
+    private var onewaysUnavailable: [Edge] = []
 
     static func initialState() -> [[T.Edge]] {
         T.Edge.allCases.combinations().filter { $0.count >= 1 }
@@ -68,10 +68,7 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
 
     override var entropy: Int {
         wallsVariations
-            .map { variation in
-                let filtered = variation.filter { !unavailable.contains($0) }
-                return filtered.isEmpty ? 0 : 1 << filtered.count - 1
-            }
+            .map { 1 << $0.count - 1 }
             .reduce(0, +)
     }
 
@@ -94,7 +91,7 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
             wallsVariations = wallsVariations.filter { $0.contains(edge) }
         case .fieldEdge(let edge):
             wallsVariations = wallsVariations.filter { $0.contains(edge) }
-            unavailable.append(edge)
+            appendOnewaysUnavailable(edge)
         case .passage(let edge):
             wallsVariations = wallsVariations.filter { !$0.contains(edge) }
         }
@@ -102,7 +99,7 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
 
     override func applySpecificRestriction(_ restriction: any ElementRestriction) {
         if let noOneWay = restriction as? OneWayRestriction<T> {
-            unavailable.append(noOneWay.edge)
+            appendOnewaysUnavailable(noOneWay.edge)
         }
     }
 
@@ -113,9 +110,11 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
     override func waveFunctionCollapse() -> T.Field.Element? {
         guard let variation = wallsVariations.randomElement() else { return nil }
         let passages = T.Edge.allCases.filter { !variation.contains($0) }
-        let filtered = variation.filter { !unavailable.contains($0) }
+        let filtered = variation.filter { !onewaysUnavailable.contains($0) }
 
-        guard !filtered.isEmpty else { return nil }
+        guard !filtered.isEmpty else {
+            return nil
+        }
 
         let onewayMaxMask = 1 << filtered.count
         let onewayMask = Int.random(in: 1..<onewayMaxMask)
@@ -123,5 +122,10 @@ final class OneWayHolderSuperposition<T: Topology>: TopologyBasedElementSuperpos
         let holder = OneWayHolder<T>(passages: passages, oneways: oneways)
 
         return holder as? T.Field.Element
+    }
+
+    private func appendOnewaysUnavailable(_ edge: Edge) {
+        onewaysUnavailable.append(edge)
+        wallsVariations = wallsVariations.filter { !onewaysUnavailable.contains($0) }
     }
 }
