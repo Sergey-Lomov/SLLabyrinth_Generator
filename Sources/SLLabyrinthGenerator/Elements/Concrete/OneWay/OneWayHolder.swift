674dc8ba0dcc57964c9a2a897de821f4d5618581
serhii.lomov <serhii.lomov@itomy.ch>
2025-03-28 17:48:10 +0200

    Refactored project structure

---
+++ Sources/SLLabyrinthGenerator/Elements/Concrete/OneWay/OneWayHolder.swift
@@ -0,0 +1,62 @@
+//
+//  OneWayHolder.swift
+//  SLLabyrinthIOS
+//
+//  Created by serhii.lomov on 20.03.2025.
+//
+
+import Foundation
+
+/// A labyrinth element with two entrances on opposite sides, allowing movement in only one direction.
+final class OneWayHolder<T: Topology>: EdgeBasedElement<T> {
+    typealias Point = T.Point
+    typealias Edge = T.Edge
+
+    let incomes: [Edge]
+    let outgoings: [Edge]
+    let walls: [Edge]
+
+    init(passages: [Edge], incomes: [Edge], outgoings: [Edge] ) {
+        self.incomes = incomes
+        self.outgoings = outgoings
+
+        let notWalls = passages + incomes + outgoings
+        self.walls = Edge.allCases.filter { !notWalls.contains($0) }
+
+        super.init(passages: passages)
+    }
+
+    override func connectedPoints(_ point: T.Point) -> [T.Point] {
+        (passages + outgoings).map {
+            T.nextPoint(point: point, edge: $0)
+        }
+    }
+
+    override func outcomeRestrictions<F>(point: Point, field: F) -> OutcomeRestrictions where F : TopologyField {
+        Edge.allCases
+            .map { restriction(point: point, edge: $0) }
+            .toDictionary()
+    }
+
+    private func restriction(point: Point, edge: Edge) -> (Point, [ElementRestriction]) {
+        let next = T.nextPoint(point: point, edge: edge)
+        let adapted = T.adaptToNextPoint(edge)
+
+        var restriction: ElementRestriction? = nil
+        if incomes.contains(edge) {
+            restriction = OneWayRestriction<T>(edge: adapted, direction: .outgoing)
+        } else if outgoings.contains(edge) {
+            restriction = OneWayRestriction<T>(edge: adapted, direction: .income)
+        } else if passages.contains(edge) {
+            restriction = TopologyBasedElementRestriction<T>.passage(edge: adapted)
+        } else {
+            restriction = TopologyBasedElementRestriction<T>.wall(edge: adapted)
+        }
+
+        guard let restriction = restriction else {
+            return (next, [])
+        }
+        let onlyRequired = OnlyRequiredOnewaysRestriction()
+        return (next, [restriction, onlyRequired])
+    }
+}
---
+++ Sources/SLLabyrinthGenerator/Elements/Concrete/OneWay/OneWayRestrictions.swift
@@ -0,0 +1,31 @@
+//
+//  OneWayRestrictions.swift
+//  SLLabyrinthIOS
+//
+//  Created by serhii.lomov on 28.03.2025.
+//
+
+import Foundation
+
+enum OnewayDirection {
+    case income, outgoing
+}
+
+final class OneWayRestriction<T: Topology>: EdgeBasedElementRestriction, IdHashable {
+    typealias Edge = T.Edge
+
+    var allowUnhandled: Bool { false }
+
+    let id = UUID().uuidString
+    let edge: Edge
+    let direction: OnewayDirection
+
+    init(edge: Edge, direction: OnewayDirection) {
+        self.edge = edge
+        self.direction = direction
+    }
+}
+
+final class OnlyRequiredOnewaysRestriction: ElementRestriction {
+    var allowUnhandled: Bool { true }
+}
